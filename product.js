1e1dd356f2b383c422335e2684e8b8d71382e37b
Adam Frewin <43453625+ajfrewin@users.noreply.github.com>
2020-01-21 23:06:11 -0500

    product & order models

---
+++ product.js
@@ -0,0 +1,11 @@
+// Schema -> model for a product
+const mongoose = require('mongoose');
+
+const productSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    name: {type: String, required: true},
+    price: {type: Number, required: true },
+    productImage: {type: String, require: true}
+});
+
+module.exports = mongoose.model('Product', productSchema);
